e4659684529175477938f4d866eca0e2b180ff8c
GShaKi <65718150+GShaKi@users.noreply.github.com>
2023-05-01 16:59:53 +0330

    Update participation

--- db.py
+++ db.py
@@ -5,11 +5,14 @@ conn = sqlite3.connect('PollDB.db')
 
 def addPoll(poll: poll.Poll) -> None:
     options = ""
+    votes = ""
     for i in poll.options:
         options += i
+        votes += str(0)
         if i != poll.options[-1]:
             options += ','
-    conn.execute("INSERT INTO Poll VALUES(?, ?, ?)", (poll.id, poll.title, options, ))
+            votes += ','
+    conn.execute("INSERT INTO Poll VALUES(?, ?, ?, ?)", (poll.id, poll.title, options, votes, ))
     conn.commit()
     
 def getPolls() -> list:
@@ -22,22 +25,29 @@ def getPolls() -> list:
         polls.append(poll.Poll(p[0], p[1], options))
     return polls
 
-def participate(pollid: int, option: int):
+def participate(pollid: int, option: int) -> None:
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM Poll")
     db_polls = cursor.fetchall()
     for p in db_polls:
         if p[0] == pollid:
-            try:
-                conn.execute(f"ALTER TABLE Poll ADD COLUMN Option{option} 'INTEGER'")
-                conn.commit()
-            except:
-                pass
-            conn.execute(f"UPDATE Poll SET Option{option} = Option{option}+1 WHERE ID = (?)", (pollid, ))
+            options: list[str] = p[3].split(',')
+            votes = ""
+            for i in range(len(options)):
+                if i == option-1:
+                    votes += str(int(options[i])+1)
+                    if i != len(options)-1:
+                        votes += ','
+                else:
+                    votes += options[i]
+                    if i != len(options)-1:
+                        votes += ','
+            conn.execute("UPDATE Poll SET Votes = (?) WHERE ID = (?)", (votes, pollid, ))
             conn.commit()
+            break
 
 def addUser(user: poll.User) -> None:
-    conn.execute("INSERT INTO User VALUES(?, ?, ?)", (user.id, user.email, user.password))
+    conn.execute("INSERT INTO User VALUES(?, ?, ?)", (user.id, user.email, user.password, ))
     conn.commit()
 
 def getUsers() -> list:
